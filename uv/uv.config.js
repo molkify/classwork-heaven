d67ff3dcb53ebfdffb4bb2b7a330356cfae1025d
molki <robertbarcelay@gmail.com>
2024-06-10 09:34:38 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -6,9 +6,9 @@ self.__uv$config = {
 
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: "/uv/uv.handler.js",
-  client: "/uv/uv.client.js",
-  bundle: "/uv/uv.bundle.js",
-  config: "/uv/uv.config.js",
-  sw: "/uv/uv.sw.js",
+  handler: "uv/uv.handler.js",
+  client: "uv/uv.client.js",
+  bundle: "uv/uv.bundle.js",
+  config: "uv/uv.config.js",
+  sw: "uv/uv.sw.js",
 };
